eb8e332da2502298531568bed295018b0931671b
Pavel Krimont <kripalser@ya.ru>
2022-03-30 19:16:32 +0600

    Add HTML minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,23 @@
+const htmlMinifier = require('html-minifier');
+
 module.exports = (config) => {
     config.addPassthroughCopy('./src/assets/css');
     config.addWatchTarget('./src/assets/css/');
 
     config.setBrowserSyncConfig({
         open: 'local',
+    });
+
+    config.addTransform('htmlMinifier', function (content) {
+        if (this.outputPath && this.outputPath.endsWith('.html')) {
+            return htmlMinifier.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+            });
+        }
+
+        return content;
     });
 
     return {
