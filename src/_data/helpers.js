9294be3d0d0c230bafe1fe1b0a10bf3acf887046
Pavel Krimont <kripalser@ya.ru>
2022-04-21 21:13:53 +0600

    Add basic sticky footer

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -26,4 +26,10 @@ module.exports = {
     getThumbSize(format = FORMAT_DEFAULT) {
         return FORMATS_AND_SIZES[format.replace('×', 'x')];
     },
+
+    // https://gist.github.com/znechai/1b25d0ee9a92e5b879175ab4f040dbbc
+    pluralize(count, words) {
+        const cases = [2, 0, 1, 1, 1, 2];
+        return `${count} ${words[(count % 100 > 4 && count % 100 < 20) ? 2 : cases[Math.min(count % 10, 5)]]}`;
+    },
 };
