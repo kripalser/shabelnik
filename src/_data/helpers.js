348658194a31024c3892c7884a775ef0b030b30f
Pavel Krimont <kripalser@ya.ru>
2023-04-21 11:51:19 +0200

    Add statuses

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -22,9 +22,21 @@ const FORMATS_AND_SIZES = {
 };
 const FORMAT_DEFAULT = '24×18';
 
+const STATUSES = [
+    'found', // Someone else's scan has been found
+    'purchased', // Purchased, but not delivered yet
+    'owned', // In possession
+    'scanned', // Scanned but not processed yet
+    'processed', // Ready to be added to the website
+];
+
 module.exports = {
     getThumbSize(format = FORMAT_DEFAULT) {
         return FORMATS_AND_SIZES[format.replace('×', 'x')];
+    },
+
+    getStatuses() {
+        return STATUSES;
     },
 
     // https://gist.github.com/znechai/1b25d0ee9a92e5b879175ab4f040dbbc
