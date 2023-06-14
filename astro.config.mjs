b246281e495a1d11720718009e70ad7097244a8a
Pavel Krimont <kripalser@ya.ru>
2023-06-14 23:49:56 +0200

    Enable HTML minification

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    compressHTML: true,
     site: 'https://shabelnik.kripalser.com',
     trailingSlash: 'always',
 });
