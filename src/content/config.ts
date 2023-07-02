3815188b9133d0d427f1560b324d9380023d769e
Pavel Krimont <kripalser@ya.ru>
2023-07-02 14:51:14 +0200

    Add custom year validation to Zod

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,9 @@
 import { defineCollection, z } from 'astro:content';
 
+const year = z.custom<number>((val) => {
+    return /(?:19|20)[0-9]{2}/.test(val as string);
+}, 'Input should be a year');
+
 // Type-check frontmatter using a schema
 export const collections = {
     books: defineCollection({
@@ -7,7 +11,7 @@ export const collections = {
             title: z.string(),
             author: z.string().optional(),
             publisher: z.string(),
-            year: z.number(), // Todo: validate year
+            year: year,
             isbn: z.string().optional(),
             format: z.string(),
             status: z.array(z.string()).optional(),
