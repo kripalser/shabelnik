72e1bd821c9c690c6fd911f162cad3b14a7c5807
Pavel Krimont <kripalser@ya.ru>
2023-07-06 18:06:21 +0200

    Update some statuses

--- src/data/statuses.ts
+++ src/data/statuses.ts
@@ -2,6 +2,7 @@ export const statuses = {
     found: 'Найдено', // Someone else's scan has been found
     purchased: 'Куплено', // Purchased, but not delivered yet
     owned: 'Есть в наличии', // In possession
+    pictures: 'Картинки', // Scanned pictures
     scanned: 'Отсканировано', // Scanned but not processed yet
     processed: 'Обработано', // Ready to be added to the website
 };
