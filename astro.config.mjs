f63ee6eb9685961903f218a7881e0b3213c4f1a8
Pavel Krimont <kripalser@ya.ru>
2023-08-31 23:10:55 +0200

    Upgrade Astro to 4.11.0 and Sass to 1.77.6, add Typescript peer dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    compressHTML: true,
     site: 'https://shabelnik.kripalser.com',
     trailingSlash: 'always',
 });
